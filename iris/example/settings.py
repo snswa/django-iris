d5f1b921a832f6321264370508d55284a799e898
Matthew Scott <gldnspud@gmail.com>
2011-01-02 19:15:42 -0800

    Use just iris.example for both example and tests project configuration; iris.tests.settings will load iris.tests.example

--- iris/example/settings.py
+++ iris/example/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'example.urls'
+ROOT_URLCONF = 'iris.example.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
