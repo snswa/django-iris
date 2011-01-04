a773d5d1c64128cbfd60076acc56d064bcb4fc84
Matthew Scott <gldnspud@gmail.com>
2011-01-04 13:03:12 -0800

    Redirect / to /iris/ in example app

--- iris/example/urls.py
+++ iris/example/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
+    url(
+        regex=r'^$',
+        view=redirect_to,
+        kwargs=dict(
+            url='/iris/',
+        ),
+    ),
     url(
         regex=r'^admin/',
         view=include(admin.site.urls),
